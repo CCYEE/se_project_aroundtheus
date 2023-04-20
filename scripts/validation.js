e2340fc9369fd455fe4491f5842ae259c067fbc7
CCYEE <ccyee4@gmail.com>
2023-04-19 21:57:23 -0700

    added validation script

---
+++ scripts/validation.js
@@ -0,0 +1,88 @@
+// const formElement = document.querySelectorAll(".modal__form");
+// const formInputElement = formElement.querySelectorAll(".form__input");
+
+function showInputError(formElement, inputElement, options) {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.add(options.inputErrorClass);
+  errorMessageElement.textContent = inputElement.validationMessage;
+  errorMessageElement.classList.add(options.errorClass);
+}
+
+function hideInputError(formElement, inputElement, options) {
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.remove(options.inputErrorClass);
+  errorMessageElement.textContent = "";
+  errorMessageElement.classList.remove(options.errorClass);
+}
+
+function checkInputValidity(formElement, inputElement, options) {
+  if (!inputElement.validity.valid) {
+    return showInputError(formElement, inputElement, options);
+  }
+  hideInputError(formElement, inputElement, options);
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputElement) => inputElement.validity.valid);
+}
+
+// FIXME:
+// function disableButton(button, {inactiveButtonClass}) {
+//   button.classList.remove(inactiveButtonClass);
+//   button.disabled = false;
+// }
+// function enableButton(button) {
+//   button.classList.add(inactiveButtonClass);
+//   button.disabled = false;
+// }
+
+function toggleButtonState(
+  inputElements,
+  submitButton,
+  { inactiveButtonClass }
+) {
+  if (hasInvalidInput(inputElements)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+  }
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function setEventListeners(formElement, options) {
+  const { inputSelector } = options;
+  const inputElements = [...formElement.querySelectorAll(inputSelector)];
+  const submitButton = formElement.querySelector(".form__save-button");
+  inputElements.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, options);
+      toggleButtonState(inputElements, submitButton, options);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, options);
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__save-button",
+  inactiveButtonClass: "form__save-button_disabled",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__error_visible",
+};
+
+enableValidation(config);
